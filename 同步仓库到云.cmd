0f1747ee039ed7dea2862ad5673b291dcb91c63b
2000PJL <1985343015@qq.com>
2021-11-21 10:20:30 +0800

    bat鎵瑰鐞嗚嚜鍔ㄦ帹閫?2021/11/21,10:20:29

--- 同步仓库到云.cmd
+++ 同步仓库到云.cmd
@@ -1,6 +1,6 @@
 :: 自动提交
 git add . 
- git commit -m "bat批处理自动推送:%date:~0,10%,%time:~0,8%" 
+git commit -m "bat批处理自动推送:%date:~0,10%,%time:~0,8%" 
 ::  git commit -m "%commitMessage%" 
 git push STM32 master
 @echo 已经完成
