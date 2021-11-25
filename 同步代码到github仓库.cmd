2f74895fbb6b5864d6b6a9c84a6b388a03a1e587
2000PJL <1985343015@qq.com>
2021-11-25 14:49:53 +0800

    作业

--- 同步代码到github仓库.cmd
+++ 同步代码到github仓库.cmd
@@ -1,6 +1,7 @@
 :: 自动提交
 git add . 
-git commit -m "bat auto push:%date:~0,10%,%time:~0,8%" 
-::  git commit -m "%commitMessage%" 
+set /p var="Please into commit message: "
+@echo git commit -m "bat auto push:%date:~0,10%,%time:~0,8%" 
+@git commit -m  "%VAR%"
 git push STM32 master
 @echo 已经完成
